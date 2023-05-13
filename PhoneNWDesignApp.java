b6ec534697c496d7b5a088990e2fb47ad0365136
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-05-13 20:42:14 +0300

    Update PhoneNWDesignApp.java

--- PhoneNWDesignApp.java
+++ PhoneNWDesignApp.java
@@ -14,11 +14,12 @@ public class PhoneNWDesignApp {
     public static void main(String[] args) throws FileNotFoundException {
 
       
+      
         KruskalAlg k = new KruskalAlg();
         BluePrintsGraph PhLNetwork = new BluePrintsGraph();
         MSTAlgorithm.graph = PhLNetwork;
-        pmh f2 = new pmh();
-        
+
+         pmh f2 = new pmh();
         // long variables to store the strating time and ending time 
         //of processing Kruskal and Prim algorithms
         long startTime, endTime;
@@ -29,24 +30,24 @@ public class PhoneNWDesignApp {
 
        MSTAlgorithm.graph.readGraphFromFile("input.txt");
        k.kruskal(MSTAlgorithm.graph.vertices);
-//       
-       f2.primAlgorithm(MSTAlgorithm.graph.vertices);       
+       
+       f2.prim(MSTAlgorithm.graph.vertices);       
 
-//        
-      MSTAlgorithm.graph.makeGraph(100, 200);
+       
+      MSTAlgorithm.graph.makeGraph(10, 20);
      
-//        // computing the starting time for the algorithm
+        // computing the starting time for the algorithm
        startTime =  System.currentTimeMillis() ;
-//  
-//        // calling prim Algorithm        
-       f2.primAlgorithm(MSTAlgorithm.graph.vertices);
-//        // computing the ending time for the algorithm
+ 
+       // calling prim Algorithm        
+       f2.prim(MSTAlgorithm.graph.vertices);
+        // computing the ending time for the algorithm
         endTime = System.currentTimeMillis() ;
-//
-//        // computing the time it took the algorithm to process
+
+        // computing the time it took the algorithm to process
        result = (double) (endTime - startTime) ;
-//
-//        //print a message
+
+       //print a message
        System.out.println("It took me " + result + " ms\n");
 
         
