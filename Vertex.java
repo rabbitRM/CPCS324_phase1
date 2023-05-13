01189068fcd340ccda21097a6cc3c87b3bd69d32
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-05-13 19:48:01 +0300

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -4,24 +4,51 @@ package GraphFramework;
 import java.util.LinkedList;
 
 public class Vertex {
-    
+ 
+    //--------------------------------------------------------------------------------
+    //defining the needed data fields 
     public char label ;
     public boolean isVisited ;
     public LinkedList<Edge> adjLists = new LinkedList<>();
     
+    
+    //--------------------------------------------------------------------------------
+    //defining the constructors
+    public Vertex(){
+    }
+ 
     public Vertex(char label ){
         this.label = label ;
         isVisited = false ;
         
     }
+    
+    //--------------------------------------------------------------------------------
+    // defining the setter and getter for data fields
+    public LinkedList<Edge> getEdges() {
+        return adjLists;
+    }
+ 
+    public boolean getVisited() {
+        return isVisited ; 
+    }
+ 
+    public void setLabel(char label) {
+        this.label = label;
+    }
+
+    public void setIsVisited(boolean isVisited) {
+        this.isVisited = isVisited;
+    }
+
+    public void setAdjLists(LinkedList<Edge> adjLists) {
+        this.adjLists = adjLists;
+    }
     public Vertex(){
         
     }
     
-    public LinkedList<Edge> getEdges() {
-        return adjLists;
-    }
-    
+  // defining the method to display the name of the vertex (which office)
     public char displayInfo (){
         return label ;
     }
